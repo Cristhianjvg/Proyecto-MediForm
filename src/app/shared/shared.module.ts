d507d3a6b92a81492ad32444f567726f7bb20aa0
mapaute2 <mapaute2@utpl.edu.ec>
2023-01-29 19:59:39 -0500

    bb

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule
@@ -16,6 +18,7 @@ import { FooterComponent } from './footer/footer.component';
   exports: [
     NavbarComponent,
     FooterComponent,
+    HeaderComponent
   ]
 })
 export class SharedModule { }
